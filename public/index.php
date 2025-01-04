afebed8cf3cb0036a2dfca479f6f167fb9ceb91f
dc7web <rafaellgiordanoo@gmail.com>
2025-01-04 16:53:27 -0300

    update 04/01

--- public/index.php
+++ public/index.php
@@ -2,9 +2,12 @@
 
 /**
  * USAR O COMANDO ABAIXO PARA INICIAR O 
- * SERVIDOR NAR PORTA 5000 NA PASTA PUBLIC
+ * SERVIDOR NA PORTA 5000 NA PASTA PUBLIC
  * EM SEU TERMINAL
  * 
  * Em seu terminal de comando:
  * php -S localhost:5000 -t public
+ * 
+ * Obs: caso a porta 5000 esteja em uso pode
+ * usar outra portal. (Ex: 8000)
  */
