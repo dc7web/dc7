a135e954f71ba7b040c93bebab1524921f73c1cb
dc7web <rafaellgiordanoo@gmail.com>
2025-01-04 16:51:16 -0300

    update 04/01

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * USAR O COMANDO ABAIXO PARA INICIAR O 
+ * SERVIDOR NAR PORTA 5000 NA PASTA PUBLIC
+ * EM SEU TERMINAL
+ * 
+ * Em seu terminal de comando:
+ * php -S localhost:5000 -t public
+ */
